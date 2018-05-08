52c6b45859edd2ed19b84c89b0050de2947bf385
Raaycc <joaoneeto2@hotmail.com>
2018-05-08 17:53:35 -0300

    usuario criado

--- src/main/java/br/edu/fapce/nexti/api/entities/Usuario.java
+++ src/main/java/br/edu/fapce/nexti/api/entities/Usuario.java
@@ -1,5 +1,58 @@
 package br.edu.fapce.nexti.api.entities;
 
-public class Usuario {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario implements Serializable {
+	
+	
+	private Long id;
+	private String email;
+	private String senha;
+	
+	public Usuario() {
+	}
+
+	@Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Column(name = "email", nullable = false)
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Column(name = "senha", nullable = false)
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", email=" + email + ", senha=" + senha;
+	}
 
 }
+
