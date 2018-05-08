66cb4a4f44b687f9de1afd859f8459e0b847f604
rayc <joaoneeto2@hotmail.com>
2018-05-08 18:59:09 -0300

    Update Biblioteca.java

--- src/main/java/br/edu/fapce/nexti/api/entities/Biblioteca.java
+++ src/main/java/br/edu/fapce/nexti/api/entities/Biblioteca.java
@@ -1,1 +1,47 @@
+package br.edu.fapce.nexti.api.entities;
 
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "biblioteca")
+public class Biblioteca implements Serializable {
+	
+	
+	private Long id;
+	private String nome;
+	
+	public Biblioteca() {
+	}
+
+	@Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Column(name = "nome", nullable = false)
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", nome=" + nome + "];
+	}
+
+}
