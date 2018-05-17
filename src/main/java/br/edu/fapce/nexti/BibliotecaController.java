312df5842fc1b99b33fd06be78f65b9f621fb676
Gabriel Barros <gabriel.o.ce@gmail.com>
2018-05-17 17:28:29 -0300

    organizando controllers

---
+++ src/main/java/br/edu/fapce/nexti/BibliotecaController.java
@@ -0,0 +1,26 @@
+package br.edu.fapce.nexti;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.edu.fapce.nexti.model.Biblioteca;
+import br.edu.fapce.nexti.repository.BibliotecaRepository;
+
+@Controller
+public class BibliotecaController {
+
+	@Autowired
+	private BibliotecaRepository repository;
+	
+	@RequestMapping("/dashboard")
+	public String biblioteca(Model model) {
+		Iterable<Biblioteca> bibliotecas = repository.findAll();
+
+		model.addAttribute("bibliotecas", bibliotecas);
+
+		return "dashboard";
+	}
+	
+}
